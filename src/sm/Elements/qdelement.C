d55f1f9e55e79d8e552088f601bb40946b9620f0
mpalaversa <marin.palaversa@fsb.hr>
2021-08-30 19:06:48 +0200

    Implementation of the "Nastran's" element coordinate system definition for the QdElement class (for a general 3D case).

--- src/sm/Elements/qdelement.C
+++ src/sm/Elements/qdelement.C
@@ -74,27 +74,78 @@ QdElement::computeGtoLRotationMatrix()
         FloatArray node4 = this->giveNode(4)->giveCoordinates();
         FloatArray e1, e2, e3;
 
+        //std::vector< FloatArray > locCoords = giveNodeCoordinates();
+
         switch (csClass)
         {
         case CSClass::Nastran:
         {
-            // Vectors spanned bewteen nodes 1 and 2, 1 and 3 and 2 and 4 respectively.
-            FloatArray v12, v13, v24;
+            /*
+                In order to obtain three unit vectors (e1, e2 and e3) according to definition of the "Nastran's coordinate system", we start by first defining
+                an element coordinate system in the "standard OOFEM" manner. This coord. sys. will be referred to as the "native OOFEM" coord. sys. and all variables
+                related with such a system will be denoted by "O".
+            */
+            // Vectors spanned bewteen nodes 1 and 2 and 1 and 3 respectively.
+            FloatArray v12, v13;
+            // v12 is a vector in the x-direction of the element coord. sys. when it is defined as the native OOFEM coord. sys. (e.g. we could call it vXO).
             v12.beDifferenceOf(node2, node1);
             v13.beDifferenceOf(node3, node1);
+
+            // Vectors in direction of y and z axes of the element coord. sys. fixed to the plane of the element and defined as the native OOFEM coord. sys.
+            FloatArray vZO, vYO;
+            vZO.beVectorProductOf(v12, v13);
+            vYO.beVectorProductOf(vZO, v12);
+
+            /*
+                Another coordinate system that we will be making use of here is the global coordinate system. Variables related to vectors defined in this system
+                will not have any special endings in this code (e.g. x, y, z, etc.).
+            */
+
+            // Unit vectors in direction of the axes of the global coord. syst.
+            FloatArray x, y, z;
+            x.resize(3);
+            x.at(1) = 1.0;
+            y.resize(3);
+            y.at(2) = 1.0;
+            z.resize(3);
+            z.at(3) = 1.0;
+
+            // Transformation matrix from the global to the coord. sys. fixed to the element's plane.
+            FloatMatrix transMatGlobToEl;
+            transMatGlobToEl.resize(3, 3);
+            transMatGlobToEl.at(1, 1) = v12.dotProduct(x) / v12.computeNorm();
+            transMatGlobToEl.at(1, 2) = v12.dotProduct(y) / v12.computeNorm();
+            transMatGlobToEl.at(1, 3) = v12.dotProduct(z) / v12.computeNorm();
+            transMatGlobToEl.at(2, 1) = vYO.dotProduct(x) / vYO.computeNorm();
+            transMatGlobToEl.at(2, 2) = vYO.dotProduct(y) / vYO.computeNorm();
+            transMatGlobToEl.at(2, 3) = vYO.dotProduct(z) / vYO.computeNorm();
+            transMatGlobToEl.at(3, 1) = vZO.dotProduct(x) / vZO.computeNorm();
+            transMatGlobToEl.at(3, 2) = vZO.dotProduct(y) / vZO.computeNorm();
+            transMatGlobToEl.at(3, 3) = vZO.dotProduct(z) / vZO.computeNorm();
+
+            /*
+                The third set of variable names that we will be using here is related to vectors given in the element's coordinate system when it is defined
+                according to the native OOFEM coord. sys. These will end with "El".
+            */
+            
+            // Vectors spanned bewteen nodes 1 and 2, 1 and 3 and 2 and 4 respectively in the coord. sys. fixed to the element's plane.
+            FloatArray v12El, v13El, v24, v24El;
+            v12El.beProductOf(transMatGlobToEl, v12);
+            v13El.beProductOf(transMatGlobToEl, v13);
             v24.beDifferenceOf(node4, node2);
+            v24El.beProductOf(transMatGlobToEl, v24);
 
-            // Find angles between vectors v12 and v13 (beta) and between vectors v12 and v24 (gamma).
+            // Find angles between vectors v12El and v13El (\beta) and between vectors v12EL and v24El (\gamma).
             double cosBeta{ 0.0 }, cosGamma{ 0.0 };
-            FloatArray v12Temp;
-            v12Temp.beScaled(-1.0, v12);
-            cosBeta = (v12.dotProduct(v13)) / (v12.computeNorm() * v13.computeNorm());
-            cosGamma = (v12Temp.dotProduct(v24)) / (v12.computeNorm() * v24.computeNorm());
+            FloatArray v12ElTemp;
+            v12ElTemp.beScaled(-1.0, v12El);
+            cosBeta = (v12El.dotProduct(v13El)) / (v12El.computeNorm() * v13El.computeNorm());
+            cosGamma = (v12ElTemp.dotProduct(v24El)) / (v12El.computeNorm() * v24El.computeNorm());
 
-            // Calculate angle between the x-axis and vC3.
+            // Calculate angle between the x-axis of the elment coord. sys. and vC3El.
             double alpha = (acos(cosBeta) + acos(cosGamma)) / 2;
 
-            // Calculate vector that is coincident with the x-axis.
+            // Calculate vector that is coincident with the x-axis of the element coord. sys.
             FloatMatrix rotMat;
             rotMat.resize(3, 3);
             rotMat.at(1, 1) = cos(-alpha);
@@ -107,19 +158,25 @@ QdElement::computeGtoLRotationMatrix()
             rotMat.at(3, 2) = 0.0;
             rotMat.at(3, 3) = 1.0;
 
-            FloatArray vX;
-            vX.beProductOf(rotMat, v13);
+            // Vector vXEl is coincident with the x-axis of the new ("Nastran's") coord. sys.
+            FloatArray vXEl;
+            vXEl.beProductOf(rotMat, v13El);
 
-            // Calculate vector coincident with the z-axis.
-            FloatArray vZ;
-            vX.resizeWithValues(3);
-            vZ.beVectorProductOf(vX, v13);
+            // Calculate vector coincident with the z-axis of the element ("Nastran's") coord. sys.
+            FloatArray vZEl;
+            vZEl.beVectorProductOf(vXEl, v13El);
 
-            // Calculate vector coincident with th y-axis.
-            FloatArray vY;
-            vY.beVectorProductOf(vZ, vX);
+            // Calculate vector coincident with th y-axis of the element ("Nastran's") coord. sys.
+            FloatArray vYEl;
+            vYEl.beVectorProductOf(vZEl, vXEl);
 
-            // Calculate unit vectors in the direction of the x, y and z axis.
+            // Vectors in direction of the element ("Nastran's") coord. sys. axes given in the global coordinate system.
+            FloatArray vX, vY, vZ;
+            vX.beTProductOf(transMatGlobToEl, vXEl);
+            vY.beTProductOf(transMatGlobToEl, vYEl);
+            vZ.beTProductOf(transMatGlobToEl, vZEl);
+
+            // Calculate unit vectors in the direction of the x, y and z axis of the element.
             vX.normalize();
             e1.copySubVector(vX, 1);
             vY.normalize();
